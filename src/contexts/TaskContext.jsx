d37d66e9feccb8227c9cd318d7e4039e20d159bf
Augusto Chaves Fiore <augustochavesfiore@gmail.com>
2024-04-08 19:59:59 -0300

    Actualizar contexto de tareas para almacenar en localStorage

--- src/contexts/TaskContext.jsx
+++ src/contexts/TaskContext.jsx
@@ -1,13 +1,27 @@
 import React, { createContext, useReducer } from "react";
 import { taskReducer } from "../reducers/taskReducer";
+import { useEffect } from "react";
 
 export const TaskContext = createContext();
 export const useTaskContext = () => React.useContext(TaskContext);
+let initialTasks = localStorage.getItem("tasks");
+if (initialTasks) {
+  try {
+    initialTasks = JSON.parse(initialTasks);
+  } catch (error) {
+    console.error("Error parsing tasks from localStorage", error);
+    initialTasks = { tasks: [] };
+  }
+} else {
+  initialTasks = { tasks: [] };
+}
 export const TaskProvider = ({ children }) => {
-  const [taskState, dispatch] = useReducer(taskReducer, { tasks: [] });
+  const [taskState, dispatch] = useReducer(taskReducer, initialTasks);
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(taskState));
+  }, [taskState]);
 
   const addTask = (task) => {
-
     dispatch({ type: "ADD_TASK", payload: task });
   };
   const updateTask = (task) => {
